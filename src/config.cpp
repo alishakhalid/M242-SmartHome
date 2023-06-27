64058a6195405c6d4248d4318314716bd8d7d8e6
alishakhalid <aalishakhalid@gmail.com>
2023-06-27 10:53:42 +0200

    added project and modified configuration

---
+++ src/config.cpp
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// mqtt configuration (username, password not used)
+const char *  mqtt_server = "cloud.tbz.ch";
+const char *  clientId = "khalid-mohammad-smart_home-1";
+const char *  username = "";
+const char *  password = "";
+
+// WiFi configuration
+const char * ssid = "Working";
+const char * passphrase = "ylmp1748";
+
+#endif // BASE_H
